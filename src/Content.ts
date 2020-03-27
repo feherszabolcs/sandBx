6374bfa5021e5d7af3a78d4dcdcd130da080f83a
feherszabolcs <feher.szabolcs1@jedlik.students.eu>
2020-03-27 12:52:11 +0100

    second

--- src/Content.ts
+++ src/Content.ts
@@ -25,6 +25,7 @@ export default class Content {
 
         // Kezd a kódolást innen -->
         res.write("<h2>Hello</h2>");
+        res.write("by.: Szabolcs");
         // <---- Fejezd be a kódolást
 
         res.write("</pre></form></body></html>");
